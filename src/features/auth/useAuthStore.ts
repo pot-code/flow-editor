449401c8268f0e0e858b47d06c4343c35ce98318
pot-code <136290354@qq.com>
2024-05-30 18:20:43 +0800

    fix: login redirection

--- src/features/auth/useAuthStore.ts
+++ src/features/auth/useAuthStore.ts
@@ -1,11 +1,16 @@
+import { User } from "oidc-client-ts"
 import { create } from "zustand"
 
 type State = {
   isAuthenticated: boolean
+  user: User | null
   setIsAuthenticated: (isAuthenticated: boolean) => void
+  setUser: (user: User | null) => void
 }
 
 export default create<State>((set) => ({
   isAuthenticated: false,
+  user: null,
+  setUser: (user) => set({ user }),
   setIsAuthenticated: (isAuthenticated) => set({ isAuthenticated }),
 }))
--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,19 +1,29 @@
 import useAuthStore from "@/features/auth/useAuthStore"
 import zitadel from "@/lib/auth/zitadel"
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, redirect } from "@tanstack/react-router"
+import { isNil } from "lodash-es"
 
 export const Route = createFileRoute("/callback")({
   component: Callback,
 })
 
 function Callback() {
-  const { isAuthenticated, setIsAuthenticated } = useAuthStore()
+  const { isAuthenticated, setIsAuthenticated, setUser } = useAuthStore()
 
   useEffect(() => {
     if (!isAuthenticated) {
       zitadel.userManager.signinRedirectCallback().then((user) => {
-        console.log("🚀 ~ zitadel.userManager.signinRedirectCallback ~ user:", user)
         setIsAuthenticated(true)
+        setUser(user)
+        redirect({ to: "/" })
+      })
+    } else {
+      zitadel.userManager.getUser().then((user) => {
+        if (isNil(user)) {
+          setIsAuthenticated(false)
+        }
+        setUser(user)
+        redirect({ to: "/" })
       })
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
