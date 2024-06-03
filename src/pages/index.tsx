1764e9cf2b957d234b5e3b995a0e449eea1b11b6
pot-code <136290354@qq.com>
2024-06-03 09:29:20 +0800

    feat: search input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import { ResizeWidth } from "@/components/resizable"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
 import Loading from "@/components/ui/loading"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { useToast } from "@/components/ui/use-toast"
 import useAuthStore from "@/features/auth/useAuthStore"
 import FlowList from "@/features/dashboard/flow-list"
@@ -11,7 +13,7 @@ import { DEFAULT_FLOW_NAME } from "@/features/flow/config"
 import zitadel from "@/lib/auth/zitadel"
 import { delayedPromise } from "@/utils/promise"
 import time from "@/utils/time"
-import { Plus } from "@phosphor-icons/react"
+import { MagnifyingGlass, Plus } from "@phosphor-icons/react"
 import { useMutation } from "@tanstack/react-query"
 import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router"
 
@@ -76,18 +78,23 @@ export default function Home() {
           <div className="h-full border-r" />
         </ResizeWidth>
         <div className="flex-1 p-4 min-h-0">
-          <section className="flex mb-8 justify-between">
+          <section className="flex mb-6 justify-between">
             <Button color="primary" onClick={onCreateFlow}>
               <Plus className="mr-2" />
               新建流程
             </Button>
-            {/* <div className="flex gap-2">
-              <Input startContent={<MagnifyingGlass />} />
-              <Tabs defaultSelectedKey="grid" color="default" variant="light">
-                <Tab key="grid" title={<GridFour />} />
-                <Tab key="list" title={<List />} />
+            <div className="flex gap-2">
+              <Tabs defaultValue="grid">
+                <TabsList>
+                  <TabsTrigger value="grid">Grid</TabsTrigger>
+                  <TabsTrigger value="list">List</TabsTrigger>
+                </TabsList>
               </Tabs>
-            </div> */}
+              <Input />
+              <Button className="flex-shrink-0" variant="secondary" size="icon">
+                <MagnifyingGlass />
+              </Button>
+            </div>
           </section>
           <FlowList />
         </div>
