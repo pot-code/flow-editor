3ebc57fa22a03ed578ba72f6e58d940f42706ac0
pot-code <136290354@qq.com>
2024-06-03 17:23:43 +0800

    feat: use swaggo

--- orval.config.ts
+++ orval.config.ts
@@ -2,11 +2,25 @@ import { defineConfig } from "orval"
 
 export default defineConfig({
   flow: {
-    input: "./openapi/flow.json",
-    output: "./src/api/flow.ts",
+    input: {
+      target: "http://127.0.0.1:3000/swagger/doc.json",
+      filters: {
+        tags: ["flow"],
+      },
+    },
+    output: {
+      target: "./src/api/flow.ts",
+    },
   },
   account: {
-    input: "./openapi/account.json",
-    output: "./src/api/account.ts",
+    input: {
+      target: "http://127.0.0.1:3000/swagger/doc.json",
+      filters: {
+        tags: ["account"],
+      },
+    },
+    output: {
+      target: "./src/api/account.ts",
+    },
   },
 })
