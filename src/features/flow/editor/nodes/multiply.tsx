f35e3248c6d5b7a9d99d38acc5c7518094db109f
pot-code <136290354@qq.com>
2024-06-26 18:01:29 +0800

    feat: multiply node effect

--- src/features/flow/editor/nodes/multiply.tsx
+++ src/features/flow/editor/nodes/multiply.tsx
@@ -1,15 +1,38 @@
-import { Badge } from "@/components/ui/badge"
-import { Separator } from "@/components/ui/separator"
 import { ReactShapeConfig } from "@antv/x6-react-shape"
 import { Trash, X } from "@phosphor-icons/react"
-import { ActionButton, NodeActions, NodeContainer, NodeContent, NodeHeader } from "./layout"
+import { defaultTo, multiply, reduce } from "lodash-es"
+import { combineLatest, map } from "rxjs"
+import { useDataFlowContext } from "../use-data-flow-context"
+import { ActionButton, NodeActions, NodeContainer, NodeHeader } from "./layout"
 import { NodeProps } from "./typings"
 
 // eslint-disable-next-line react-refresh/only-export-components
 function Multiply({ node }: NodeProps) {
+  const { getChannel } = useDataFlowContext()
+  const inputs = useMemo(() => node.getPortsByGroup("input").map((port) => getChannel(port.id!)), [getChannel, node])
+  const outputs = useMemo(() => node.getPortsByGroup("output").map((port) => getChannel(port.id!)), [getChannel, node])
+
   function onDelete() {
     node.remove()
   }
+
+  useEffect(() => {
+    const sub = combineLatest(inputs.map((s) => s.source))
+      .pipe(
+        map((v) =>
+          reduce(
+            v.map((i) => defaultTo(i, 0)),
+            (a, b) => multiply(a, b),
+          ),
+        ),
+      )
+      .subscribe((v: number) => {
+        outputs.forEach((s) => {
+          s.publish(v)
+        })
+      })
+    return () => sub.unsubscribe()
+  }, [inputs, outputs])
 
   return (
     <NodeContainer>
@@ -24,26 +47,56 @@ function Multiply({ node }: NodeProps) {
           <span>乘法</span>
         </div>
       </NodeHeader>
-      <Separator />
-      <NodeContent className="flex flex-col gap-2">
-        <div className="relative">
-          <div className="px-4">
-            <Badge variant="default">Input: 空数据</Badge>
-          </div>
-        </div>
-        <div className="relative">
-          <div className="px-4">
-            <Badge variant="default">Input: 空数据</Badge>
-          </div>
-        </div>
-      </NodeContent>
     </NodeContainer>
   )
 }
 
 export default {
+  component: Multiply,
   shape: "multiply",
   effect: ["data"],
   width: 150,
-  component: Multiply,
+  ports: {
+    groups: {
+      output: {
+        position: "right",
+        attrs: {
+          circle: {
+            magnet: true,
+            fill: "#000",
+            stroke: "none",
+            r: 5,
+          },
+        },
+      },
+      input: {
+        position: "left",
+        attrs: {
+          circle: {
+            magnet: true,
+            fill: "#000",
+            stroke: "none",
+            r: 5,
+          },
+        },
+      },
+    },
+    items: [
+      {
+        group: "output",
+        args: {
+          dy: 18,
+        },
+      },
+      {
+        group: "input",
+      },
+      {
+        group: "input",
+        args: {
+          dy: 36,
+        },
+      },
+    ],
+  },
 } as ReactShapeConfig
